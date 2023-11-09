cc1bdffc4de68f1a36c4b3bf89b610c9293d4651
Anka98 <anka98-98@mail.ru>
2023-11-09 17:46:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import datetime as dt
+
+FORMAT='%d.%m.%Y %H:%M'
+data = {}
+
+#добавляем событие и автоматически ставим отмеку о том, что событие не выполнено
+def add_data(data_time, new_events):
+    data_time = dt.datetime.strptime(data_time, FORMAT)
+    flag = False
+    data[data_time] = new_events, flag
+    return data
+    
+#просмотр всех запланированных событий    
+def viewing_data(data):
+    for data_t in data:
+        print(f'На {data_t} запланировано {data[data_t]} событие')
+
+def delete_data_events(data):
+    data_time = input('Введите дату и время в формате: ДД:ММ:ГГ Ч:М')
+    data_time = dt.datetime.strptime(data_time, FORMAT)
+    if data_time in data.keys():
+        del data[data_time]
+    else:
+        print('На эту дату у вас ничего не запланировано')
+    return data
+#изменение событий(без проверки отметки о выполнении)
+def edit_data(data):#, new_events, flug):
+    data_time = input('Введите дату и время в формате: ДД:ММ:ГГ Ч:М')
+    data_time = dt.datetime.strptime(data_time, FORMAT)
+    if data_time in data.keys():
+        data[data_time] = input('Введите новое событие')
+    else:
+        print('На эту дату у вас ничего не запланировано')
+    return data
+    
+print(add_data('25.03.2020 22:30', 'asd'))
+print(add_data('26.03.2020 22:30', 'asd'))
+
+#print(delete_data_events(data))
+#print(edit_data(data))
+#viewing_data(data)
+def menu():
+    print('Пожалуйста, выберете действие')
+    print(f'Если вы хотите добавить новую запись, введите ')
